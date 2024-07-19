22ce81f28b831dab25628dd9f0d006b088b4e8fe
Patrick Cowland <patrickcowland@pingidentity.com>
2024-07-19 15:42:49 +0100

    code to reproduce the error

--- internal/resource/config/example_resource.go
+++ internal/resource/config/example_resource.go
@@ -3,11 +3,15 @@ package config
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
@@ -25,8 +29,15 @@ type exampleResource struct {
 }
 
 type exampleResourceModel struct {
-	Id        types.String `tfsdk:"id"`
-	StringVal types.String `tfsdk:"string_val"`
+	Id              types.String `tfsdk:"id"`
+	StringVal       types.String `tfsdk:"string_val"`
+	SingleNestedVal types.Object `tfsdk:"single_nested_val"`
+}
+
+type exampleSingleNestedValResourceModel struct {
+	StringVal        types.String `tfsdk:"string_val"`
+	StringValStatic  types.String `tfsdk:"string_val_static"`
+	StringValDynamic types.String `tfsdk:"string_val_dynamic"`
 }
 
 // GetSchema defines the schema for the resource.
@@ -46,6 +57,31 @@ func (r *exampleResource) Schema(ctx context.Context, req resource.SchemaRequest
 				Description: "Optional string attribute",
 				Optional:    true,
 			},
+			"single_nested_val": schema.SingleNestedAttribute{
+				Description: "Optional single nested object",
+				Optional:    true,
+
+				Attributes: map[string]schema.Attribute{
+					"string_val": schema.StringAttribute{
+						Description: "Required string attribute",
+						Required:    true,
+					},
+
+					"string_val_static": schema.StringAttribute{
+						Description: "Computed single nested attribute object, using \"UseStateForUnknown()\" plan modifier",
+						Computed:    true,
+
+						PlanModifiers: []planmodifier.String{
+							stringplanmodifier.UseStateForUnknown(),
+						},
+					},
+
+					"string_val_dynamic": schema.StringAttribute{
+						Description: "Computed single nested attribute object, without using \"UseStateForUnknown()\" plan modifier",
+						Computed:    true,
+					},
+				},
+			},
 		},
 	}
 }
@@ -55,56 +91,97 @@ func (r *exampleResource) Metadata(_ context.Context, req resource.MetadataReque
 	resp.TypeName = req.ProviderTypeName + "_example"
 }
 
-func (m *exampleResourceModel) SetId() {
-	m.Id = types.StringValue("id")
-}
-
 func (r *exampleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var plan exampleResourceModel
+	var plan, state exampleResourceModel
 
-	diags := req.Plan.Get(ctx, &plan)
-	resp.Diagnostics.Append(diags...)
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Set computed id
-	plan.SetId()
-	diags = resp.State.Set(ctx, plan)
-	resp.Diagnostics.Append(diags...)
+	// Stub API
+	data := plan
+	data.SetId()
+
+	resp.Diagnostics.Append(state.toState(ctx, data)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
 }
 
 func (r *exampleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var state exampleResourceModel
+	var data, state exampleResourceModel
 
-	diags := req.State.Get(ctx, &state)
-	resp.Diagnostics.Append(diags...)
+	// Stub API
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Set computed id
-	state.SetId()
-	diags = resp.State.Set(ctx, state)
-	resp.Diagnostics.Append(diags...)
+	data = state
+
+	resp.Diagnostics.Append(state.toState(ctx, data)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
 }
 
 // Update updates the resource and sets the updated Terraform state on success.
 func (r *exampleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var plan exampleResourceModel
+	var plan, state exampleResourceModel
 
-	diags := req.Plan.Get(ctx, &plan)
-	resp.Diagnostics.Append(diags...)
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Set computed id
-	plan.SetId()
-	diags = resp.State.Set(ctx, plan)
-	resp.Diagnostics.Append(diags...)
+	// Stub API
+	data := plan
+	data.SetId()
+
+	resp.Diagnostics.Append(state.toState(ctx, data)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
 }
 
 // No backend so no logic needed
 func (r *exampleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
+
+func (r *exampleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func (m *exampleResourceModel) SetId() {
+	m.Id = types.StringValue("id")
+}
+
+func (m *exampleResourceModel) toState(ctx context.Context, data exampleResourceModel) (diags diag.Diagnostics) {
+	m.Id = data.Id
+	m.StringVal = data.StringVal
+
+	singleNestedObjectTyp := map[string]attr.Type{
+		"string_val":         types.StringType,
+		"string_val_static":  types.StringType,
+		"string_val_dynamic": types.StringType,
+	}
+
+	var d diag.Diagnostics
+
+	singleNestedPlan := exampleSingleNestedValResourceModel{
+		StringVal:        types.StringValue("required string"),
+		StringValStatic:  types.StringValue("service static string"),
+		StringValDynamic: types.StringValue("service dynamic string"),
+	}
+
+	if !data.SingleNestedVal.IsNull() {
+		d = data.SingleNestedVal.As(context.Background(), &singleNestedPlan, basetypes.ObjectAsOptions{})
+		diags.Append(d...)
+		if diags.HasError() {
+			return
+		}
+
+		singleNestedPlan.StringValStatic = types.StringValue("service static string")
+		singleNestedPlan.StringValDynamic = types.StringValue("service dynamic string, updated because this block is defined")
+	}
+
+	m.SingleNestedVal, d = types.ObjectValueFrom(ctx, singleNestedObjectTyp, singleNestedPlan)
+	diags = append(diags, d...)
+
+	return
 }
